a9c0d30f1e7eb7506bf80c1fa4ea7dfd3984e74f
Sandie Emonts <118970227+sandie75@users.noreply.github.com>
2025-02-14 00:15:42 +0100

    css fix

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,18 @@ const Projects = () => {
     return <section id="portfolio" className ="projects">
          <h2>Projets personnels</h2>
          <p>Voici quelques projets sur lesquels j'ai travaillé récemment.</p>
+         <div className="projectsItems">
+            <div className="project">
+
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+
+            </div>
+
+         </div>
 
          <button className="otherProjects">Mes autres projets <span className="arrow">→</span></button>
         

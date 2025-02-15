7f3602c4447accfb929e6b77fccbe95b16f23a8b
Sandie Emonts <118970227+sandie75@users.noreply.github.com>
2025-02-15 18:13:58 +0100

    fix css projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,13 @@
 import './projects.css';
 
 const Projects = () => {
-    return <section id="portfolio" className ="projects">
+    return (
+    <section id="portfolio" className ="projects">
          <h2>Projets personnels</h2>
          <p>Voici quelques projets sur lesquels j'ai travaillé récemment.</p>
+
          <div className="projectsItems">
+            {/*Projet 1 Concert*/}
             <div className="project">
                 <div className="project-img">
                     <a href="#" target="_blank">
@@ -12,6 +15,7 @@ const Projects = () => {
                     </a>
                 </div>
                 <div className="project-text">
+                    <h3>Salle de concert</h3>
                     <p>blablablaablablabalbalablababalbalbabalablababalalbabalalbab</p>
                 </div>
                 <div className="project-tech">
@@ -24,21 +28,58 @@ const Projects = () => {
                 </div>
                 <button className="project-see">Voir le projet</button>
             </div>
+
+            {/*Projet 2 Quiz */}
             <div className="project">
-            <div className="project-img"></div>
-                <div className="project-text"></div>
-                <div className="project-tech"></div>
-                <button className="project-see"></button>
-            </div>
+                <div className="project-img">
+                    <a href="#" target="_blank">
+                        <img src="/images/quiz-2x1.png" alt="aperçu du site"/>
+                    </a>
+                </div>
+                <div className="project-text">
+                    <h3>Quiz Game of Thrones</h3>
+                    <p>albalabalbalbalbalbalababalalbalbalbalalbabllaba</p>
+                </div>
+                <div className="project-tech">
+                    <ul>
+                        <li>Html/Css</li>
+                        <li>React</li>
+                        <li>API</li>
+                    </ul>
+                </div>
+                <button className="project-see">
+                        Voir le projet
+                </button>
+            </div>    
+
+            {/*Projet 3 Mastermind */}
             <div className="project">
-
+                <div className="project-img">
+                    <a href="#" target="_blank">
+                        <img src="/images/mastermind-2x1.png" alt="aperçu du site"/>
+                    </a>
+                </div>
+                <div className="project-text">
+                        <h3>Mastermind</h3>
+                        <p>albalabalbalbalbalbalababalalbalbalbalalbabllaba</p>
+                </div>
+                <div className="project-tech">
+                        <ul>
+                            <li>Html/Css</li>
+                            <li>React</li>
+                        </ul>
+                </div>
+                <button className="project-see">
+                        Voir le projet
+                </button>  
             </div>
-
-         </div>
-
-         <button className="otherProjects">Mes autres projets <span className="arrow">→</span></button>
-        
+        </div>
+         <button className="otherProjects">
+            Mes autres projets 
+            <span className="arrow">→</span>
+         </button>
     </section>
-}
+);
+};
 
 export default Projects;

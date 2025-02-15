77d6a65832b31f6eb685497d6055234b669f965d
Sandie Emonts <118970227+sandie75@users.noreply.github.com>
2025-02-15 18:58:20 +0100

    fix zoom débordement

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return <section className ="about">
         <h1>Bonjour, c'est Sandie.</h1>
          <h2>Développeuse web basée à Montpellier.</h2>  
-         <p className="intro">Bienvenue sur mon portfolio ! J'aime construire des interfaces simples et efficaces. Après un bac +2 en développement full stack d'applications web et mobiles, j'ai fait quelques projets.</p> 
+         <p className="intro">Bienvenue sur mon portfolio ! J'aime construire des interfaces simples et efficaces. Après un bac +2 en développement d'applications web et mobiles, j'ai fait quelques projets.</p> 
          
          <button className="cv">Télécharger mon CV <span className="arrow">→</span></button>
         

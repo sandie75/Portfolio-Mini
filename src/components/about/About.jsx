b237622056ea5923b97edea61b2d3b8f250839e4
Sandie Emonts <118970227+sandie75@users.noreply.github.com>
2025-02-15 23:17:37 +0100

    links feature, download pdf

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,14 +4,16 @@ const About = () => {
     return <section className ="about">
         <h1>Bonjour, c'est Sandie.</h1>
          <h2>Développeuse web.</h2>  
-         <p className="intro">Bienvenue sur mon portfolio ! J'aime construire des interfaces simples et efficaces. Après un bac +2 en développement d'applications web et mobiles, j'ai fait quelques projets.</p> 
+         <p className="intro">Bienvenue sur mon portfolio&nbsp;! J'aime construire des interfaces simples et efficaces. Après un bac +2 en développement d'applications web et mobiles, j'ai fait quelques projets.</p> 
          
-         <button className="cv">
-                        Télécharger mon CV 
-                        <span>
-                            <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
-                        </span>
-        </button>
+        <a href="/cv.pdf" download="CV_Sandie_Emonts" className="cv-download-link">
+            <button className="cv">
+                            Télécharger mon CV 
+                            <span>
+                                <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
+                            </span>
+            </button>
+        </a>
         
     </section>
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,15 +9,25 @@ const Projects = () => {
          <div className="projectsItems">
             {/*Projet 1 Concert*/}
             <div className="project">
+
                 <div className="project-img">
-                    <a href="#" target="_blank">
-                        <img src="/images/concert-2x1.png" alt="aperçu du site"/>
+
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+
+                        <picture>
+                            <source srcSet="/images/concert-square.png" media="(max-width: 768px)"/>  
+                            <img src="/images/concert-2x1.png" alt="aperçu du site"/>
+                        </picture>
+
                     </a>
+
                 </div>
+
                 <div className="project-text">
                     <h3>Salle de concert</h3>
                     <p>blablablaablablabalbalablababalbalbabalablababalalbabalalbab</p>
                 </div>
+
                 <div className="project-tech">
                     <ul>
                         <li>Html/Css</li>
@@ -26,25 +36,36 @@ const Projects = () => {
                         <li>SQL</li>
                     </ul>
                 </div>
-                <button className="project-see">
+
+                <button className="project-see" onClick={() => window.open('https://sandieemonts.sites.3wa.io/Concerts/index.php','_blank')}>
                         Voir le projet 
                         <span>
                             <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
                         </span>
                 </button>
+
             </div>
 
             {/*Projet 2 Quiz */}
             <div className="project">
                 <div className="project-img">
-                    <a href="#" target="_blank">
-                        <img src="/images/quiz-2x1.png" alt="aperçu du site"/>
+
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+
+                        <picture>
+                            <source srcSet="/images/quiz-square.png" media="(max-width: 768px)"/>  
+                            <img src="/images/quiz-2x1.png" alt="aperçu du site"/>
+                        </picture>
+
                     </a>
+
                 </div>
+
                 <div className="project-text">
                     <h3>Quiz Game of Thrones</h3>
                     <p>albalabalbalbalbalbalababalalbalbalbalalbabllaba</p>
                 </div>
+
                 <div className="project-tech">
                     <ul>
                         <li>Html/Css</li>
@@ -52,47 +73,67 @@ const Projects = () => {
                         <li>API</li>
                     </ul>
                 </div>
-                <button className="project-see">
+
+                <button className="project-see" onClick={() => window.open('https://sandie75.github.io/Quiz-Got/', '_blank')}>
                         Voir le projet 
                         <span>
                             <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
                         </span>
                 </button>
+
             </div>    
 
             {/*Projet 3 Mastermind */}
             <div className="project">
+
                 <div className="project-img">
-                    <a href="#" target="_blank">
-                        <img src="/images/mastermind-2x1.png" alt="aperçu du site"/>
+
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+
+                        <picture>
+                            <source srcSet="/images/mastermind-square.png" media="(max-width: 768px)"/>  
+                            <img src="/images/mastermind-2x1.png" alt="aperçu du site"/>
+                        </picture>
+
                     </a>
                 </div>
+
                 <div className="project-text">
                         <h3>Mastermind</h3>
                         <p>albalabalbalbalbalbalababalalbalbalbalalbabllaba</p>
                 </div>
+
                 <div className="project-tech">
                         <ul>
                             <li>Html/Css</li>
                             <li>React</li>
                         </ul>
                 </div>
-                <button className="project-see">
+
+                <button className="project-see" onClick={() => window.open('https://sandie75.github.io/Mastermind/', '_blank')}>
                         Voir le projet 
                         <span>
                             <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
                         </span>
                 </button>  
+
             </div>
         </div>
-        <button className="otherProjects">
+
+        <button className="otherProjects" onClick={() => window.open('https://github.com/sandie75', '_blank')}>
                         Autres projets 
                         <span>
                             <img className="arrow" src="/icons/chevronBlanc.png" alt="icon"/>
                         </span>
         </button>
+
     </section>
 );
 };
 
 export default Projects;
+
+/*target="_blank": Ouvre le lien dans un nouvel onglet.
+rel="noopener noreferrer": Si tu ouvres un lien dans un nouvel onglet, la page ouverte peut potentiellement accéder à ta page actuelle via window.opener. C'est une faille de sécurité.
+noopener: Empêche la nouvelle page d’accéder à window.opener (évite certaines attaques).
+noreferrer: Empêche d’envoyer l’info de l’origine (referer) au site cible.*/

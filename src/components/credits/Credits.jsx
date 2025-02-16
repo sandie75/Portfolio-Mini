efc1a585696beffa1137b87978d5b7ab6e68b200
Sandie Emonts <118970227+sandie75@users.noreply.github.com>
2025-02-16 20:43:17 +0100

    feature download code source

--- src/components/credits/Credits.jsx
+++ src/components/credits/Credits.jsx
@@ -3,7 +3,9 @@ import './credits.css';
 const Credits = () => {
     return <section className ="credits">
         <p>2025 Sandie Emonts</p>
-        <a href="#">Voir le code source</a>
+        <a href="https://github.com/sandie75/Portfolio-Mini" target="_blank" rel="noopener noreferrer">
+        Voir le code source
+        </a>
     </section>
 }
 
